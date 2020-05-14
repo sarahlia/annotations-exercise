b61caca4fa77c1c2614c0a7780d7ecf4ebf7804b
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-14 15:53:07 -0500

    edit after walkthrough

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
-    @SuppressWarnings("deprecation")
+//    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
         Author author = new Author("Sandi", "Metz");
+        System.out.println("author.fullName() = " + author.fullName());
 
         author.addBook("Practical Object-Oriented Design in Ruby");
         author.addBook("99 Bottles of OOP");
 
-        for (String book: author.getBooks()) {
+        for (String book: author.publishedBooks()) {
             System.out.println(book);
         }
     }
